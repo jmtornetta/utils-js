14b8c62b615d1887c155d956e354734f1db447ac
Jon Tornetta <jmtornetta@gmail.com>
2022-08-03 01:05:47 -0400

    Feature: Add 'throttle' function.

--- pure.js
+++ pure.js
@@ -54,6 +54,27 @@ export const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+Throttles callback function executions to one per timeout limit.  
+@param {Function} ms  Callback function
+@param {Number} ms  Milliseconds
+@param {Any} args Arguments for function
+@example
+throttle(console.log, 1000, 'first click')
+throttle(console.log, 1000, 'second click')
+throttle(console.log, 1000, 'third click')
+*/
+const throttle = (() => {
+    let runs = 0
+    return (fn, ms, ...args) => {
+      runs += 1
+      const currentRun = runs
+      setTimeout(() => {
+        if (currentRun === runs) fn(...args)
+      }, ms)
+    }
+  })()
+
 export const click_selector = (sel, el = document) => el.querySelector(sel).click(); // Clicks a given selector on a given element.
 
 export const find_classFromNodelist = (_class, ...els) => { // Returns a node with a given class from a node list.
