09eb0ac23129d2b6c08be6be5e23d53987141114
Jon Tornetta <jmtornetta@gmail.com>
2022-08-11 22:33:55 -0400

    Refactor: Return array of filenames instead of objects. Filepath can be derived.

--- node/ls.js
+++ node/ls.js
@@ -2,14 +2,9 @@ import * as fs from "node:fs/promises"
 
 const ls = async (path) => {
   // Return all files from a given directory path in an array of objects
-  const out = []
+  const filenames = []
   const dir = await fs.opendir(path) // Need to 'await' if running 'fs' async
-  for await (const _file of dir) {
-      const _obj = {}
-      _obj.filename = _file.name // File name property
-      _obj.filepath = path + _file.name // Full file path propery
-      out.push(_obj) // Store file object in array of files to return
-  }
-  return(out)
+  for await (const _file of dir) filenames.push(_file.name) // If calling 'ls' on 'path' the filepath can be derived and is unnecessary.  
+  return filenames
 }   
 export default ls;
