d41ed4e272dcf26032d2df76913e3cf116b7da68
Jon Tornetta <jmtornetta@gmail.com>
2022-08-29 18:02:41 -0400

    Fix: Use 'replace' to handle leading spaces

--- getLuminosity.ts
+++ getLuminosity.ts
@@ -1,10 +1,8 @@
 function getLuminosity(hexColor:string):number {
-  const _color = hexColor.substring(1);      // strip #
-  const rgb = parseInt(_color, 16);   // convert rrggbb to decimal
+  // Values greater than '150' are light.
+  const rgb = parseInt(hexColor.replace(/^\s*#/,""), 16);   // Strip # and leading spaces and convert rrggbb to decimal
   const r = (rgb >> 16) & 0xff;  // extract red
   const g = (rgb >>  8) & 0xff;  // extract green
   const b = (rgb >>  0) & 0xff;  // extract blue
-  
-  // Values less than '40' are dark.
   return 0.2126 * r + 0.7152 * g + 0.0722 * b; // Per ITU-R BT.709
 }
