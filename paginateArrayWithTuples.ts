cbcbd7deeb455e4acb673d9db17b16932c05b143
Jon Tornetta <jmtornetta@gmail.com>
2023-10-16 17:20:14 -0400

    [ft,tweak] useWindowDimensions react hook; rename 'react.ts'

--- paginateArrayWithTuples.ts
+++ paginateArrayWithTuples.ts
@@ -1,6 +1,6 @@
 import { Tuple } from './utilTypes'
 
-export default function paginateArrayWithTuples<TData extends Record<string, any>, TNumber extends number>(data: TData[], itemsPerPage: TNumber){
+export default function paginateArrayWithTuples<TData extends Record<string, any>, TNumber extends number>(data: TData[], itemsPerPage: TNumber) {
   return data.reduce((acc, curr, i) => {
     if (i === acc.length - 1) return acc
     const indexForSlide = i % itemsPerPage
